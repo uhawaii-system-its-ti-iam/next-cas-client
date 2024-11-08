f018038bcef41fd1ca0c65db0b1f879a234445b5
Jordan Wong <jordanw4@hawaii.edu>
2024-11-07 16:58:46 -1000

    fix: Jest unable to find /app & /pages modules

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     format: ['cjs', 'esm'],
     entry: ['./src/index.ts', './src/app.ts', './src/pages.ts'],
+    target: 'node18',
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
