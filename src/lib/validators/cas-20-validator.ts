2b3c6f34b34f0f75de2aa313d3182ffb0b27c91f
Mohamad Khawam <mk1800@rutgers.edu>
2025-09-22 13:24:26 -0400

    Added redirect login to validators

--- src/lib/validators/cas-20-validator.ts
+++ src/lib/validators/cas-20-validator.ts
@@ -5,6 +5,12 @@ import { validationUrl, Validator } from './validator';
 class Cas20Validator implements Validator {
     path = 'serviceValidate';
     service = 'service';
+    redirectUrl?: string;
+    constructor(redirectUrl?: string) {
+        if (redirectUrl) {
+            this.redirectUrl = redirectUrl;
+        }
+    }
 
     async validate(ticket: string): Promise<CasUser> {
         return await validate(validationUrl(this), ticket);
--- src/lib/validators/cas-30-validator.ts
+++ src/lib/validators/cas-30-validator.ts
@@ -5,7 +5,12 @@ import { validationUrl, Validator } from './validator';
 class Cas30Validator implements Validator {
     path = 'p3/serviceValidate';
     service = 'service';
-
+    redirectUrl?: string;
+    constructor(redirectUrl?: string) {
+        if (redirectUrl) {
+            this.redirectUrl = redirectUrl;
+        }
+    }
     async validate(ticket: string): Promise<CasUser> {
         return await validate(validationUrl(this), ticket);
     }
